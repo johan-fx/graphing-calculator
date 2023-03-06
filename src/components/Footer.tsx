bb51baebeb4e186ec6c9c9c10a8d29c27643e36e
Johan Sosa Salazar <sosa.johan@gmail.com>
2023-03-06 19:44:52 +0100

    feat: Added leemons logo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,11 @@ export const Footer: React.FunctionComponent = () => {
   return (
     <footer className="footer">
       <a className="icon" href="/">
-        <Icon />
+        <img
+          src="https://s3.eu-west-1.amazonaws.com/global-assets.leemons.io/logo_leemons_bb2ece3956.svg"
+          alt="Leemons"
+          height={20}
+        />
       </a>
       <div className="links">
         <div className="item title">Graphing Calculator </div>
